a6e800eba9832e4a02e18e56805f277231f66b7a
xlakostax <konstantin.veselovskii@gmail.com>
2019-10-17 16:10:26 +0300

    RC2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Main from './Main'
 import NavBar from './NavBar'
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AppBar, createMuiTheme, makeStyles, Typography } from '@material-ui/core';
+import { AppBar, Box, createMuiTheme, makeStyles, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#fff'
+      main: '#fff', text: '#FF0000'
     },
     secondary: {
       main: '#000'
@@ -21,7 +22,8 @@ const useStyles = makeStyles(theme => ({
     display: "block",
     position: "relative",
     fontFamily: "'Raleway', sans-serif",
-    fontWeight: 400
+    fontWeight: 400,
+    padding: "0 5px"
   }
 }));
 
@@ -39,9 +41,15 @@ const App = () => {
         </AppBar>
         <Main />
         <AppBar id="year" color="secondary" position="static" component = "footer" style = {{ display: "flex", position: "relative", width: "100%", height: "10%", justifyContent: "center", alignItems: "center", marginTop: "1em"}}>
-          <Typography gutterBottom variant = "subtitle2" component = "p" className = {classes.footer_year}>
-            &copy; The 27 Club | <Year />
-          </Typography>
+          <Box component = "div" style = {{position: "relative", display: "flex", justifyContent: "center"}}>
+            <Typography gutterBottom variant = "subtitle2" component = "p" className = {classes.footer_year}>
+              &copy; Made with
+            </Typography>
+            <FavoriteIcon style = {{color: "#FF0000"}} fontSize="small"/>
+            <Typography gutterBottom variant = "subtitle2" component = "p" className = {classes.footer_year}>
+               by "The 27 Club" | <Year />
+            </Typography>
+          </Box>
         </AppBar>
       </ThemeProvider>
     </Router>
